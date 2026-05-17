b2393189c3ae79cd0992efef6a742660a8faafe6
hneemann <helmut.neemann@arcor.de>
2026-05-17 09:58:01 +0200

    changed the pdf library

--- go.mod
+++ go.mod
@@ -6,13 +6,15 @@ toolchain go1.24.2
 
 require (
 	github.com/hneemann/session v0.0.0-20260227071414-d6ce4281640c
-	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/signintech/gopdf v0.36.0
 	github.com/stretchr/testify v1.11.1
 	golang.org/x/image v0.36.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/phpdave11/gofpdi v1.0.14-0.20211212211723-1f10f9844311 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sigurn/crc16 v0.0.0-20240131213347-83fcde1e29d1 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
--- scanner/pdf.go
+++ scanner/pdf.go
@@ -1,7 +1,7 @@
 package scanner
 
 import (
-	"github.com/jung-kurt/gofpdf"
+	"github.com/signintech/gopdf"
 	"io"
 )
 
@@ -11,19 +11,28 @@ type PdfImage struct {
 }
 
 func CreatePDF(w io.WriteCloser, path ...PdfImage) error {
-	pdf := gofpdf.New("P", "mm", "A4", "")
+	defer w.Close()
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
 	for _, p := range path {
 		pdf.AddPage()
 
 		if p.Height > (p.Width*12)/10 {
 			//210 x 297
-			pdf.Image(p.Name, 0, 0, 210, 297, false, "", 0, "")
+			err := pdf.Image(p.Name, 0, 0, &gopdf.Rect{W: 210 / 25.4 * 72, H: 297 / 25.4 * 72})
+			if err != nil {
+				return err
+			}
 		} else {
 			h := float64((210 * p.Height) / p.Width)
 			y := (297 - h) / 2
 
-			pdf.Image(p.Name, 0, y, 210, h, false, "", 0, "")
+			err := pdf.Image(p.Name, 0, y/25.4*72, &gopdf.Rect{W: 210 / 25.4 * 72, H: h / 25.4 * 72})
+			if err != nil {
+				return err
+			}
 		}
 	}
-	return pdf.OutputAndClose(w)
+	_, err := pdf.WriteTo(w)
+	return err
 }
